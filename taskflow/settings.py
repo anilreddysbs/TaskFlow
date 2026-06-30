19d1b994aefb1608103a75b77013cf57f0cbc895
Anil Reddy <anilreddysbs@gmail.com>
2026-06-30 10:21:35 +0530

    Add configurable email SMTP settings with fallback to console backend in debug mode

--- taskflow/settings.py
+++ taskflow/settings.py
@@ -326,3 +326,18 @@ if SENTRY_DSN:
         send_default_pii=False,
     )
 
+
+# Email Configuration
+if DEBUG and not os.environ.get('EMAIL_HOST'):
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+    EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
+    EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+    EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() in ('true', '1', 'yes')
+    EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False').lower() in ('true', '1', 'yes')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'noreply@taskflow.com')
+
+
